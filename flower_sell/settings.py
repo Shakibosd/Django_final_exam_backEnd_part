b53570ea4d333d6fcc2bceddc3a7dcd48002e814
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-04 23:10:24 +0600

    finish

--- flower_sell/settings.py
+++ flower_sell/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://django-final-exam-backend-part.onrender.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://django-final-exam-backend-part.onrender.com/','https://*.127.0.0.1']
 
 LOGIN_URL  = "http://127.0.0.1:5500/login.html"
 
