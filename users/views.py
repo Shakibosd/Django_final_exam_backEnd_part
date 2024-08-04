a225d6fa8aa19ddaf356ed0aa61f1e8e8738b894
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-05 03:01:15 +0600

    finish

--- users/views.py
+++ users/views.py
@@ -48,6 +48,9 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
+        redirect('login')
+    else:
+        redirect('register')    
     
 class LoginAPIView(APIView):
     serializer_class = LoginSerializer
