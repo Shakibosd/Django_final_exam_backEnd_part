d4f4aba7a91ab4847390a7f7bb654d8634bddf00
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-06 01:23:04 +0600

    finish

--- users/views.py
+++ users/views.py
@@ -19,44 +19,9 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# class RegisterAPIView(APIView):
-#     serializer_class = RegistrationSerializer
-
-#     def post(self, request):
-#         serializer = self.serializer_class(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             print(user)
-#             token = default_token_generator.make_token(user)
-#             uid = urlsafe_base64_encode(force_bytes(user.pk))
-#             confirm_link = f'http://127.0.0.1:8000/users/active/{uid}/{token}/'
-#             print(confirm_link)
-#             email_subject = 'Confirm Your Email'
-#             email_body = render_to_string('confirm_email.html', {'confirm_link': confirm_link})
-#             email = EmailMultiAlternatives(email_subject, '', to=[user.email])
-#             email.attach_alternative(email_body, 'text/html')
-#             email.send()
-#             return Response({'detail': 'Check your email for confirmation'}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# def activate(request, uid64, token):
-#     try:
-#         uid = urlsafe_base64_decode(uid64).decode()
-#         user = User._default_manager.get(pk=uid)
-#     except (User.DoesNotExist):
-#         user = None
-
-#     if user is not None and default_token_generator.check_token(user, token):
-#         user.is_active = True
-#         user.save()
-#         return redirect('login')
-#     else:
-#         return redirect('register') 
-   
-
 class RegisterAPIView(APIView):
     serializer_class = RegistrationSerializer
- 
+
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -73,24 +38,21 @@ class RegisterAPIView(APIView):
             email.send()
             return Response({'detail': 'Check your email for confirmation'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
+
 def activate(request, uid64, token):
     try:
-        uid = force_str(urlsafe_base64_decode(uid64))
+        uid = urlsafe_base64_decode(uid64).decode()
         user = User._default_manager.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+
+    except (User.DoesNotExist):
         user = None
- 
+
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
         return redirect('login')
     else:
-        return redirect('register')
-    
-
-
-
+        return redirect('register') 
 
 class LoginAPIView(APIView):
     serializer_class = LoginSerializer
