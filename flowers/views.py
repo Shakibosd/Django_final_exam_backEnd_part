c12c26f6d6e9efb60fce2877c1c17acffcd40250
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-08-12 12:49:00 +0600

    fubusg

--- flowers/views.py
+++ flowers/views.py
@@ -1,6 +1,4 @@
-#https://docs.google.com/spreadsheets/d/1edlnD8h-s5itNyxCbNrd8s_munC-nMslM_H0IL-QWcs/edit?gid=0#gid=0
 from rest_framework import viewsets
-from flowers.permissions import HasOrdered
 from orders.models import Order
 from .serializers import FlowerSerializer, CommentsSerializer
 from rest_framework.views import APIView
@@ -11,12 +9,10 @@ from .models import Flower, Comment
 from rest_framework import generics
 from flowers.serializers import CommentSerializer
 from django.shortcuts import get_object_or_404
-from flowers.serializers import CheckOrder
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
-from .permissions import HasOrdered
 
 class FlowerViewSet(viewsets.ModelViewSet):
     queryset = Flower.objects.all()
@@ -58,34 +54,16 @@ class CommentShowAPIView(generics.ListAPIView):
         flower = Flower.objects.get(id = postId)
         return Comment.objects.filter(flower = flower)
         
-# class CommentAPIView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         serializer = CommentSerializer(data=request.data)
-#         if serializer.is_valid():
-#             flowerId = serializer.validated_data['flowerId']
-#             flower = get_object_or_404(Flower, id=flowerId)
-#             user = request.user
-
-#             if not Order.objects.filter(user=user, flower=flower).exists():
-#                 return Response({"message": "You need to purchase the flower to comment."}, status=status.HTTP_403_FORBIDDEN)
-
-#             names = serializer.validated_data['names']
-#             body = serializer.validated_data['comment']
-#             comment = Comment.objects.create(flower=flower, name=names, body=body)
-#             comment.save()
-
-#             return Response({"message": "Comment created"}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class CommentAPIView(APIView):
-    permission_classes = [IsAuthenticated, HasOrdered]
-
     def post(self, request, *args, **kwargs):
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
             flowerId = serializer.validated_data['flowerId']
             flower = get_object_or_404(Flower, id=flowerId)
+            user = request.user
+
+            if not Order.objects.filter(user=user, flower=flower).exists():
+                return Response({"message": "You need to purchase the flower to comment."}, status=status.HTTP_403_FORBIDDEN)
 
             names = serializer.validated_data['names']
             body = serializer.validated_data['comment']
@@ -96,37 +74,21 @@ class CommentAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class CheckOrderView(APIView):
-#     # permission_classes = [IsAuthenticated]    
-
-#     def post(self, request):
-#         flowerId = request.data.get('flowerId')
-        
-#         if flowerId:
-#             try:
-#                 flower = Flower.objects.get(id=flowerId)
-#                 order_exists = Order.objects.filter(flower=flower).exists()
-#                 return Response({'order_exists': order_exists}, status=status.HTTP_200_OK)
-#             except Flower.DoesNotExist:
-#                 return Response({'error': 'Flower not found'}, status=status.HTTP_404_NOT_FOUND)
-#         else:
-#             return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
-
 class CheckOrderView(APIView):
-    permission_classes = [IsAuthenticated, HasOrdered]
+    # permission_classes = [IsAuthenticated]    
 
-    def post(self, request, *args, **kwargs):
-        serializer = CheckOrder(data=request.data)
-        if serializer.is_valid():
-            flowerId = serializer.validated_data['flowerId']
-            flower = get_object_or_404(Flower, id=flowerId)
-            user = request.user
-
-            if Order.objects.filter(user=user, flower=flower).exists():
-                return Response({"order_exists": True}, status=status.HTTP_200_OK)
-            return Response({"order_exists": False}, status=status.HTTP_403_FORBIDDEN)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def post(self, request):
+        flowerId = request.data.get('flowerId')
+        
+        if flowerId:
+            try:
+                flower = Flower.objects.get(id=flowerId)
+                order_exists = Order.objects.filter(flower=flower).exists()
+                return Response({'order_exists': order_exists}, status=status.HTTP_200_OK)
+            except Flower.DoesNotExist:
+                return Response({'error': 'Flower not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
         
 
 @api_view(['GET'])
