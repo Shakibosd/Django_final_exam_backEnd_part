64efa473ad11affe4e65b7ddcfbb68675754ca3a
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-09-11 17:15:53 +0600

    finish

--- flower_sell/settings.py
+++ flower_sell/settings.py
@@ -170,10 +170,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SITE_ID = 1
-
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL')
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
