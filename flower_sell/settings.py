c9f3b5e5bb42a9322dcb9e9c2280db5e3357caf9
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-09-30 20:21:23 +0600

    vercel added

--- flower_sell/settings.py
+++ flower_sell/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 LOGIN_URL  = "http://127.0.0.1:5500/login.html"
 
